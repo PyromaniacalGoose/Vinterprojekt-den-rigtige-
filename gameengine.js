3aded9412a6dcb6acc9777b36134263dc3e839da
ExcellentUsername <mikkelmoellerjensen04@gmail.com>
2022-12-12 09:25:37 +0100

    new functions
    Added the functions checkPlayerWinstate() and checkAIWinstate

--- gameengine.js
+++ gameengine.js
@@ -21,31 +21,44 @@ class board {
 
         }
 
-        this.checkWinstate = function(){ //checks if there's a valid win state
+        this.checkPlayerWinstate = function(){ //checks if there's a valid win state
 
             for(var i = 0; i < rows - 3; i++){
 
                 for(var j = 0; j < coloums - 3; j++){
 
-                    if(boardArray[i][j].this.tile.playerPiece == true && boardArray[i][j + 1] && boardArray[i][j + 2] && boardArray[i][j + 3]){ //vertically 4 in a row
+                    if(boardArray[i][j].this.tile.playerPiece == true && 
+                        boardArray[i][j + 1].this.tile.playerPiece == true && 
+                        boardArray[i][j + 2].this.tile.playerPiece == true && 
+                        boardArray[i][j + 3].this.tile.playerPiece == true){ //vertically 4 in a row
 
                         win
 
                     }
 
-                    else if(boardArray[i][j] && boardArray[i + 1][j] && boardArray[i + 2][j] && boardArray[i + 3][j]){ //horizontally 4 in a row
+                    else if(boardArray[i][j].this.tile.playerPiece == true && 
+                        boardArray[i + 1][j].this.tile.playerPiece == true && 
+                        boardArray[i + 2][j].this.tile.playerPiece == true && 
+                        boardArray[i + 3][j].this.tile.playerPiece == true){ //horizontally 4 in a row
 
                         win
 
                     }
 
-                    else if(boardArray[i][j] && boardArray[i + 1][j + 1] && boardArray[i + 2][j + 2] && boardArray[i + 3][j + 3]){ //slanted up 4 in a row
+                    else if(boardArray[i][j].this.tile.playerPiece == true && 
+                        boardArray[i + 1][j + 1].this.tile.playerPiece == true && 
+                        boardArray[i + 2][j + 2].this.tile.playerPiece == true && 
+                        boardArray[i + 3][j + 3].this.tile.playerPiece == true){ //slanted up 4 in a row
 
                         win
 
                     }
 
-                    else if(j >= 4 && boardArray[i][j] && boardArray[i + 1][j - 1] && boardArray[i + 2][j - 2] && boardArray[i + 3][j - 3]){ //slanted down 4 in a row
+                    else if(j >= 4 && 
+                        boardArray[i][j].this.tile.playerPiece == true && 
+                        boardArray[i + 1][j - 1].this.tile.playerPiece == true && 
+                        boardArray[i + 2][j - 2].this.tile.playerPiece == true && 
+                        boardArray[i + 3][j - 3].this.tile.playerPiece == true){ //slanted down 4 in a row
 
                         win
 
@@ -62,6 +75,62 @@ class board {
             }
 
         }
+
+        this.checkAIWinstate = function(){ //checks if there's a valid win state
+
+            for(var i = 0; i < rows - 3; i++){
+
+                for(var j = 0; j < coloums - 3; j++){
+
+                    if(boardArray[i][j].this.tile.AIPiece == true && 
+                        boardArray[i][j + 1].this.tile.AIPiece == true && 
+                        boardArray[i][j + 2].this.tile.AIPiece == true && 
+                        boardArray[i][j + 3].this.tile.AIPiece == true){ //vertically 4 in a row
+
+                        win
+
+                    }
+
+                    else if(boardArray[i][j].this.tile.AIPiece == true && 
+                        boardArray[i + 1][j].this.tile.AIPiece == true && 
+                        boardArray[i + 2][j].this.tile.AIPiece == true && 
+                        boardArray[i + 3][j].this.tile.AIPiece == true){ //horizontally 4 in a row
+
+                        win
+
+                    }
+
+                    else if(boardArray[i][j].this.tile.AIPiece == true && 
+                        boardArray[i + 1][j + 1].this.tile.AIPiece == true && 
+                        boardArray[i + 2][j + 2].this.tile.AIPiece == true && 
+                        boardArray[i + 3][j + 3].this.tile.AIPiece == true){ //slanted up 4 in a row
+
+                        win
+
+                    }
+
+                    else if(j >= 4 && 
+                        boardArray[i][j].this.tile.AIPiece == true && 
+                        boardArray[i + 1][j - 1].this.tile.AIPiece == true && 
+                        boardArray[i + 2][j - 2].this.tile.AIPiece == true && 
+                        boardArray[i + 3][j - 3].this.tile.AIPiece == true){ //slanted down 4 in a row
+
+                        win
+
+                    }
+
+                    else {
+
+                        return;
+
+                    }
+
+                }
+
+            }
+
+        }
+
 
         this.pieceCounter = 0;
 
