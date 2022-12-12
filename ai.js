73cee161ed0d12a8d25afa7978fba05a7b1cc3d1
PyromaniacalGoose <Mathiasjohnbeck@gmail.com>
2022-12-12 09:14:32 +0100

    stuff

---
+++ ai.js
@@ -0,0 +1,34 @@
+
+class Ai {
+    
+    update(){
+
+    }
+
+     evaluateBoard(){
+        let boardData = new array = this.getBoardData(); //gets the board data from the board
+        if (boardData[0][0]) { //checks if the player is in an immediate-close-win-state
+
+        } else{
+
+        }
+        
+     }
+
+      getBoardData(){
+
+        let Check = new array; //array to return bool for immediate-close-win-state along with row
+        
+
+
+        return [Check, ];
+     }
+
+
+     evaluateBranch(node, searchhDepth){
+        
+    }
+
+}
+
+
