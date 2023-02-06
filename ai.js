22d5e77dde4824d5cd9c624c2dc1731dd685692b
PyromaniacalGoose <Mathiasjohnbeck@gmail.com>
2023-02-06 08:22:09 +0100

    weee

--- ai.js
+++ ai.js
@@ -17,11 +17,11 @@ constructor(branchSearchDepth){
 
 
 
-     getAvailableMoves(){
+     getAvailableMoves(selectedBoard){
         let avaliableMoves = new Array();
       
-        for (let i = 0; i <= board.columns; i++) { //obs fix this to coloumns when mikkel fixes his shit
-         let tempTopRow = board.boardArray[i][board.boardArray[i].length-1] //for easier syntax
+        for (let i = 0; i <= selectedBoard.columns; i++) { 
+         let tempTopRow = selectedBoard.boardArray[i][selectedBoard.boardArray[i].length-1] //for easier syntax
          if(tempTopRow[0] == false && tempTopRow[1] == false){ //checks if the top row of each coloum is empty, and therefore viable to play
            
             avaliableMoves.push(i); //if it is viable, the coloum is pushed into the avaliable moveset
@@ -33,23 +33,61 @@ constructor(branchSearchDepth){
      simulateTree(){
       dummyBoard = new Board(columns,rows,true) //creates a dummy board
 
-      let branches = this.getAvailableMoves();
+      let branches = this.getAvailableMoves(board);
       for (let i = 0; i < branches.length; o++) {
-        this.evaluateBranch(branches[i],this.branchSearchDepth); //evaluates each of the available moves
+        this.evaluateBranch(branches[i]); //evaluates each of the available moves
          
       }
      }
    
 
      evaluateBranch(node){
+      let movesets = [];
+
       dummyBoard.boardArray = board.boardArray //replicates the state of the current board to the dummyBoard
-      dummyBoard.boardArray[node].forEach(element => {
-         for (let i = 0; i <= this.branchSearchDepth; i++) {
-            
-         }
-      });
-      
+      dummyBoard.addPiece(node,1); //adds the ai piece at the node coloumn
+      if(dummyBoard.checkWinState(1)){ //checks if the ai can win in 1 move
+         movesets.push([[node],2])
+      } else {
+         this.recursion();
+
+      }
     }
+    
+    recursionPlayer(){
+      let stepAvailableMoves = this.getAvailableMoves(dummyBoard);
+      let boardState = dummyBoard.boardArray; //saves the current state of the board for recall
+      let tempMoveset = [];
+
+         for (let i = 0; i <= stepAvailableMoves; i++) {
+            dummyBoard.addPiece(stepAvailableMoves[i],0);
+            if(dummyBoard.checkWinState(0)){
+               tempMoveset[i] = [[node,i],-1];
+            } else{
+               this.recursionAI(tempMoveset[i]);
+            }
+            dummyBoard.boardArray = boardState;
+         }
+    }
+
+    recursionAI(branch){
+      let stepAvailableMoves = this.getAvailableMoves(dummyBoard);
+      let boardState = dummyBoard.boardArray; //saves the current state of the board for recall
+      let tempMoveset = []
+
+
+         for (let i = 0; i <= stepAvailableMoves; i++) {
+            dummyBoard.addPiece(stepAvailableMoves[i],1);
+            if(dummyBoard.checkWinState(1)){
+               branch[0].push(i); //pushes the path along with value of branch if ai wins
+               branch.push(1);
+            }
+
+             
+            dummyBoard.boardArray = boardState;
+         }
+      }
+
 
 }
 
